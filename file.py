9d865d90110e6861ef49d5a9c04751d24971c6b4
chiragoel <39792413+chiragoel@users.noreply.github.com>
2021-11-02 16:12:31 +0530

    Update file.py

--- file.py
+++ file.py
@@ -16,32 +16,35 @@ def task(cmd):
     
     return
 
-# Run Multiple Thread
-for i in range(1,101):
-    start = time.time()
-    print("New thread \n")
-    files = os.listdir(r"C:\Users\DELL\Desktop\multi\input")
-    for f in files:
-        cmd= f
-        t1 = threading.Thread(target=task , args=(cmd,))
-        t1.start()
+
+def main():
+    # Run Multiple Thread
+    for i in range(1,101):
+        start = time.time()
+        print("New thread \n")
+        files = os.listdir("./input")
+        for f in files:
+            cmd= f
+            t1 = threading.Thread(target=task , args=(cmd,))
+            t1.start()
+            while True:
+                if threading.activeCount()-1 <= i:
+                    break
+                time.sleep(1)
+
+        # Waiting to finish the thread
         while True:
-            if threading.activeCount()-1 <= i:
+            if threading.activeCount() == 1:
                 break
             time.sleep(1)
-
-# Waiting to finish the thread
-    while True:
-        if threading.activeCount() == 1:
-            break
-        time.sleep(1)
-        print ("Thread Left ... ",threading.activeCount() - 2)
-    end = time.time()
-    t = end - start
-    fw = open("time.csv","a")
-    writing = "%s , % f\n"%(str(i) ,t)
-    fw.write(writing)
-    start = 0
-    end = 0
-    print("\n...All Thread ends....")
-fw.close()
+            print ("Thread Left ... ",threading.activeCount() - 2)
+        end = time.time()
+        t = end - start
+        fw = open("time.csv","a")
+        writing = "%s , % f\n"%(str(i) ,t)
+        fw.write(writing)
+        start = 0
+        end = 0
+        print("\n...All Thread ends....")
+    fw.close()
+main()
